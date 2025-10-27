5f103abb2c5f7e7c8b78be8cfa3e36c888fd8cfa
Daler <isoev9733@gmail.com>
2025-10-27 13:31:43 +0400

    feat(home): добавил домашнюю страницу и компонент пустого состояния - создал Home компонент - добавил универсальный EmptyState компонент (нет данных / нет друзей / пусто) - подготовил структуру для будущих экранов - улучшил UX при отсутствии контента

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { TuiRoot } from "@taiga-ui/core";
 import {Component, inject} from '@angular/core';
 import {ThemeService} from './shared/services/theme.service';
 import {Header} from './layout/header/header';
+import {Home} from './layout/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [TuiRoot, Header],
+  imports: [TuiRoot, Header, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
