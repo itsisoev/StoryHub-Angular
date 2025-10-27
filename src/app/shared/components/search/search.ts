2fe724f498a6f51956d06c8d5b2301f3e0f23bb1
Daler <isoev9733@gmail.com>
2025-10-27 14:00:01 +0400

    feat(search): добавлен компонент пустого состояния для результатов поиска

---
+++ src/app/shared/components/search/search.ts
@@ -0,0 +1,28 @@
+import {ChangeDetectionStrategy, Component, input, signal} from '@angular/core';
+import {TuiButton, TuiDialog, TuiIcon} from '@taiga-ui/core';
+import {TuiInputModule} from '@taiga-ui/legacy';
+import {TuiAutoFocus} from '@taiga-ui/cdk';
+
+@Component({
+  selector: 'uc-search',
+  imports: [
+    TuiButton,
+    TuiIcon,
+    TuiInputModule,
+    TuiAutoFocus,
+    TuiDialog
+  ],
+  templateUrl: './search.html',
+  styleUrl: './search.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Search {
+  title = input('');
+  desc = input('')
+
+  open = signal<boolean>(false);
+
+  protected showDialog(): void {
+    this.open.set(true);
+  }
+}
